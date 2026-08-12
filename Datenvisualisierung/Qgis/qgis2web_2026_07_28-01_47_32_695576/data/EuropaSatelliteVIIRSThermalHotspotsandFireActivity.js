f8c380bbaff049a23dbe7b332a0e79ca50a60a42
EStasch <Edelgard.Stasch@ISIS-networks.de>
2026-08-12 16:17:10 +0200

    test Live Data

---
+++ Datenvisualisierung/Qgis/qgis2web_2026_07_28-01_47_32_695576/data/EuropaSatelliteVIIRSThermalHotspotsandFireActivity.js
@@ -0,0 +1,4 @@
+/* To Do: Dynamisch laden vpm Server */
+//var json_EuropaSatelliteVIIRSThermalHotspotsandFireActivity_2 = new L.GeoJSON.AJAX("data/data-fire-Europa_26_07_28.geojson", {style: style_EuropaSatelliteVIIRSThermalHotspotsandFireActivity_2, onEachFeature: pop_EuropaSatelliteVIIRSThermalHotspotsandFireActivity_2}).addTo(map);
+
+var json_EuropaSatelliteVIIRSThermalHotspotsandFireActivity_2
